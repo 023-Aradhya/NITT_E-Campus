9fa5957a13e4311174eb0c886e6a5c88529bc055
Harshit Anand <147899846+040KaundilyaHarshit@users.noreply.github.com>
2025-05-13 12:38:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-campus-nitt-frontend.vercel.app",
     methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
     credentials: true,
   })
